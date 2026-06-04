221d4fdd01dc97a5c1b805cef67f3b63c180843e
NaomiVCC05 <villalon@oregonstate.edu>
2026-06-04 11:31:21 -0700

    Added steps to python script

---
+++ plant_host.py
@@ -0,0 +1,18 @@
+#Save the excel data as a tsv file on the super computer
+
+
+
+#Read in the tsv file and save as a pandas dataframe
+
+
+
+
+#Filter the dataframe by the Plant pathgentic capacity column to retain only the leaf/fruit/seed pathogens and assign this filted data frame as a new varible
+
+
+
+#For the new dataframe, index it by the Specific_host column and get a count of the different values (summary function?)
+
+
+
+#Use these counts to make a pie chart (or stacked bar chart)
